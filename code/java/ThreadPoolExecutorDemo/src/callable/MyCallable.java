dee8c59784d85ce69131bed459e6fbec8b92c172
DW1099 <pw0035227@hihonor.com>
2022-04-11 16:00:25 +0800

    11

--- code/java/ThreadPoolExecutorDemo/src/callable/MyCallable.java
+++ code/java/ThreadPoolExecutorDemo/src/callable/MyCallable.java
@@ -7,6 +7,7 @@ public class MyCallable implements Callable<String> {
     @Override
     public String call() throws Exception {
         Thread.sleep(1000);
+        //git test 
         //返回执行当前 Callable 的线程名字
         return Thread.currentThread().getName();
     }
